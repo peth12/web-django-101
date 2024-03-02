eee702da4ea59800126de8e47a33282c91d8bd71
peth12 <pethkk12@gmail.com>
2024-03-02 08:35:34 +0700

    create template

--- web_django_app/views.py
+++ web_django_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world peth")
+    return render(request, "index.html")
 
 def about(request):
-    return HttpResponse("<h1>about me</h1>")
+    return render(request, "about.html")
 
 def form(request):
-    return HttpResponse("<h1>form save data</h1>")
+    return render(request, "form.html")
